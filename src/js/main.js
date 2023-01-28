7407f6352b7960785e03e3f6b9f1e73b9d002474
DimaChekashov <foxdima99@gmail.com>
2023-01-28 23:23:24 +0100

    implements faq

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
+const removeClasses = (elems, className) =>
+    elems.forEach((elem) => elem.classList.remove(className));
+
+function accordion() {
+    const accordions = document.querySelectorAll(".faq__item");
+
+    accordions.forEach((accordion) => {
+        accordion.querySelector(".faq__item-content").style.maxHeight = `${
+            accordion.querySelector(".faq__item-text").offsetHeight
+        }px`;
+
+        accordion.addEventListener("click", () => {
+            if (accordion.classList.contains("active")) {
+                accordion.classList.remove("active");
+            } else {
+                removeClasses(accordions, "active");
+                accordion.classList.add("active");
+            }
+        });
+    });
+}
+
 window.addEventListener("DOMContentLoaded", function () {
     $("#main-slider").owlCarousel({
         loop: true,
@@ -5,6 +27,7 @@ window.addEventListener("DOMContentLoaded", function () {
         nav: true,
         items: 1,
         dots: false,
-        navText:["",""],
+        navText: ["", ""],
     });
+    accordion();
 });
